81cc76b95ade02a2e40436de5057c03a5cf26a48
Deadskull <maheshraju@wibesapp.com>
2018-09-04 12:49:36 +0530

    Done with oject factory files today

---
+++ bucky.js
@@ -0,0 +1,6 @@
+var movies = require("./movie");
+
+BuckyFavmovie = movies;
+
+BuckyFavmovie.favMovie = "chappie";
+console.log("Bucky's fav movie is: "+BuckyFavmovie.favMovie);
---
+++ emily.js
@@ -0,0 +1,6 @@
+var movies = require("./movie");
+
+EmilyFavmovie = movies;
+
+EmilyFavmovie.favMovie = "The NOtebook";
+console.log("Emily's fav movie is: "+EmilyFavmovie.favMovie);
