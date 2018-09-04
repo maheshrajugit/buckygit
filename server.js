e6a161297c378631dc531fe60d855f077aa581de
Deadskull <maheshraju@wibesapp.com>
2018-09-04 15:13:49 +0530

    Before going to home i was at 15th nodejs video

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 var http = require('http');
 var fs = require('fs');
+var connect = require('connect');
 
-//404 response
-function send404Response(response){
-    response.writeHead(404,{"Content-Type":"text/plain"});
-    response.write("Dorkaled mama!");
-    response.end();
+var app = connect();
+
+profile = (request,response) =>{
+    console.log('User Requested profile');
 }
 
-function onRequest(request, response){
-    if( request.method == 'GET' && request.url =='/'){
-        response.writeHead(200,{"Content-Type": "text/html"});
-        fs.createReadStream("./index.html").pipe(response);
-    }
-    else{
-        send404Response(response);
-    }
+
+forum = (request,response) =>{
+    console.log('User Requested Forum');
 }
 
-http.createServer(onRequest).listen(8888);
+
+app.use('/profile',profile);
+app.use('/forum',profile);
+
+
+//Creating a server localhost:8888
+
+http.createServer(app).listen(8888);
 console.log("Server is now online...");
+
