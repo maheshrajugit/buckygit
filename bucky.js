fdd06264c9256bdecaf1e81d832fdbbddad40bbb
Deadskull <maheshraju@wibesapp.com>
2018-09-04 12:50:35 +0530

    started file system tut

--- bucky.js
+++
@@ -1,6 +0,0 @@
-var movies = require("./movie");
-
-BuckyFavmovie = movies;
-
-BuckyFavmovie.favMovie = "chappie";
-console.log("Bucky's fav movie is: "+BuckyFavmovie.favMovie);
--- emily.js
+++
@@ -1,6 +0,0 @@
-var movies = require("./movie");
-
-EmilyFavmovie = movies;
-
-EmilyFavmovie.favMovie = "The NOtebook";
-console.log("Emily's fav movie is: "+EmilyFavmovie.favMovie);
